9495141900237b59e13b846fae7c65dec08d5d41
Marcos Cunha <eng.marcos@gmail.com>
2025-09-05 18:50:31 +0200

    Add plant healthy model

--- scripts/axelera_copy_models_to_device.py
+++ scripts/axelera_copy_models_to_device.py
@@ -6,17 +6,18 @@ onnx_files = [
     "yolo11m_bowl_fountain_finetune.onnx",
     "yolo11n_bowl_fountain_finetune.onnx",
     "mobilenetv2_bowl_level_classifier.onnx",
-]
+    "mobilenetv2_fountain_level_classifier.onnx",
+    "mobilenetv2_plant_health_classifier.onnx"
+    ]
 
 user = "aetina"
 ip_address = "192.168.1.111"
 target_path = "/media/sdcard/axelera/devel/voyager-sdk/customers/models/"
 
-for file in onnx_files:
-    cmd = [
-        "scp",
-        f"{MODEL_DIR}/{file}",
-        f"{user}@{ip_address}:{target_path}"
-    ]
-    print(f"Copying {file} to {user}@{ip_address}:{target_path}")
-    subprocess.run(cmd, check=True)
+cmd = [
+    "scp",
+    *[f"{MODEL_DIR}/{file}" for file in onnx_files],
+    f"{user}@{ip_address}:{target_path}"
+]
+print(f"Copying {onnx_files} to {user}@{ip_address}:{target_path}")
+subprocess.run(cmd, check=True)
